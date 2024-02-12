a1b110790b62a1dbe00d6d941d51fd0d83934997
yuange <hzyuange@gmail.com>
2024-02-13 04:42:19 +0800

    a

--- app/src/main/java/com/example/inicokkk/MainActivity.java
+++ app/src/main/java/com/example/inicokkk/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.inicokkk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button btn_login, btn_reset;
+    public EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
+
+    private void initView(){
+        btn_login = findViewById(R.id.btn_login);
+        btn_reset = findViewById(R.id.btn_reset);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+    }
+
+    private void initListener(){
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username.setText("");
+                password.setText("");
+            }
+        });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doLogin();
+            }
+        });
+    }
+
+    private void doLogin(){
+
+    }
+
 }
