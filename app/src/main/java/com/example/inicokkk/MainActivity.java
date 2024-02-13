00ffec800fb33d9a5922ffca5a20b070ac5739f1
yuange <hzyuange@gmail.com>
2024-02-13 11:48:21 +0800

    a

--- app/src/main/java/com/example/inicokkk/MainActivity.java
+++ app/src/main/java/com/example/inicokkk/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.inicokkk;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("nixxxxxx", "onCreate");
         initView();
+        initListener();
     }
 
     private void initView() {
@@ -24,12 +32,14 @@ public class MainActivity extends AppCompatActivity {
         btn_reset = findViewById(R.id.btn_reset);
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
+        Log.i("nixxxxxx", "initView");
     }
 
     private void initListener() {
         btn_reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i("nixxxxxx", "initListener，重置");
                 username.setText("");
                 password.setText("");
             }
@@ -37,13 +47,60 @@ public class MainActivity extends AppCompatActivity {
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                doLogin();
+                Log.i("nixxxxxx", "initListener，登陆");
+                doLoginGet();
+                doLoginPost();
             }
         });
     }
 
-    private void doLogin() {
+    private void doLoginGet() {
+        Log.i("nixxxxxx", "doLoginGet");
+        String user = String.valueOf(username.getText());
+        String pass = String.valueOf(password.getText());
+        new Thread() {
+            @Override
+            public void run() {
+                Log.i("nixxxxxx", "doLoginGet-run");
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder().url("http://192.168.5.113:5001/indexg").build();
+                Call call = client.newCall(request);
+                try {
+                    Response response = call.execute();
+                    ResponseBody responseBody = response.body();
+                    String resString = responseBody.string();
+                    Log.i("nixxxxxx", resString);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("nixxxxxx", e.getMessage());
+                }
+            }
+        }.start();
+    }
 
+    private void doLoginPost() {
+        Log.i("nixxxxxx", "doLoginPost");
+        String user = String.valueOf(username.getText());
+        String pass = String.valueOf(password.getText());
+        new Thread() {
+            @Override
+            public void run() {
+                Log.i("nixxxxxx", "doLoginPost-run");
+                OkHttpClient client = new OkHttpClient();
+                FormBody formBody = new FormBody.Builder().add("username", "小溪").build();
+                Request request = new Request.Builder().url("http://192.168.5.113:5001/indexp").post(formBody).build();
+                Call call = client.newCall(request);
+                try {
+                    Response response = call.execute();
+                    ResponseBody responseBody = response.body();
+                    String resString = responseBody.string();
+                    Log.i("nixxxxxx", resString);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("nixxxxxx", e.getMessage());
+                }
+            }
+        }.start();
     }
 
 }
